ce9ecc43bb1bbe8f259440051f79ca839b22fb51
Howard <54549555+howardheaton@users.noreply.github.com>
2021-01-29 16:09:46 -0800

    Update KM.py
    Minor clean up of KM_alg

--- KM.py
+++ KM.py
@@ -30,16 +30,16 @@ class KM_alg():
         return output % (self._alpha, self._max_depth, self._eps, self._device)
  
     def __call__(self, u, d, eps=-1):                  
-        eps      = eps if eps > 0 else self._eps_def
-        depth    = 0.0       # number of iterations for entire batch to converge   
-        u_prev   = u.clone()
+        eps     = eps if eps > 0 else self._eps
+        depth   = 0.0       # number of iterations for entire batch to converge   
+        u_prev  = u.clone()
         indices = np.array(range(len(u[:,0])))
         u       = u.to(self._device)        
         # Mask identifies not converged 'nc' samples (False = converged)
         nc      = np.ones((1,u[:,0].size()[0]), dtype=bool)
         nc = nc.reshape((nc.shape[1]))
-        with torch.no_grad():  
-            # loop until all samples must converge                
+        with torch.no_grad(): # avoid storing grad in memory
+            # loop until all samples converge or max out iterations
             while nc.any() > 0 and depth < self._max_depth:                 
                 Tu         = self._T(u,d) if self._T.use_data() else T(u)
                 u_prev     = u.clone()
@@ -52,7 +52,7 @@ class KM_alg():
         return u.to(self._device), depth
 
     def apply_T(self, u, d): # Use this update for backprop
-        y = u.detach()        # Detach any gradient graphs    
+        y = u.detach()       # Detach any gradient graphs    
         return self._T(y, d)
 
     def assign_ops(self, S, T): # Update T after every optimizer training step
