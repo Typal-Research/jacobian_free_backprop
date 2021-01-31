4e65af7036414819f5205bd855820933e52ed0de
Howard <54549555+howardheaton@users.noreply.github.com>
2021-01-31 09:44:59 -0800

    Update KM.py
    Removed checks for T dependence on data. Henceforth we require T = T(u,d) and S = S(u).

--- KM.py
+++ KM.py
@@ -4,8 +4,8 @@ import torch
 class KM_alg():
     """
     Krasnoselskii Mann Algorithm (KM)
-    Purpose: Using input operators S and T, solve the problem
-             Find u \in fix( (1-alpha) * T + alpha * S).
+    Purpose: Using input operators S(u) and T(u,d), solve the problem
+             Find u  s.t.  u = (1 - alpha) * T(u; d) + alpha * S(u).
     """
     def __init__(self, S, T, alpha, device, 
                  max_depth=200, proj_tol=1.0e-9, eps=1.0e-5):
@@ -22,10 +22,6 @@ class KM_alg():
         output += '      max depth = %r\n'
         output += '      eps def   = %r\n' 
         output += '      device    = %r\n'
-        if self._T.use_data():
-            output += '      T         = T(u,d) - (signal + data)\n'     
-        else:
-            output += '      T         = T(u) - (signal only)\n'  
         output += ')'
         return output % (self._alpha, self._max_depth, self._eps, self._device)
  
@@ -41,12 +37,12 @@ class KM_alg():
         with torch.no_grad(): # avoid storing grad in memory
             # loop until all samples converge or max out iterations
             while nc.any() > 0 and depth < self._max_depth:                 
-                Tu         = self._T(u,d) if self._T.use_data() else T(u)
+                Tu         = self._T(u,d)
                 u_prev     = u.clone()
-                u          = self._alpha * self._S(u,d) + (1-self._alpha) * Tu
+                u          = self._alpha * self._S(u) + (1-self._alpha) * Tu
                 nc[nc > 0] = [torch.norm(u[i,:] - u_prev[i,:]) > eps 
                               for i in indices[nc > 0]]
-                depth     += 1.0             
+                depth     += 1.0    
         if depth >= self._max_depth:
             print("KM: Max Depth Reached - Break Forward Loop")
         return u.to(self._device), depth
