b9af0eadbb6ea2246bccfba153f8da28c516216c
Howard <54549555+howardheaton@users.noreply.github.com>
2021-02-05 07:48:00 -0800

    Made KM cmopliant with Flake8 and added docstrings

--- KM.py
+++ KM.py
@@ -5,9 +5,10 @@ import torch
 class KM_alg():
     """
     Krasnoselskii Mann (KM) Algorithm
+
     Purpose: Using input operators S(u) and T(u,d), solve the problem
              Find u  s.t.  u = (1 - alpha) * T(u; d) + alpha * S(u).
-    We assume T is a neural network operator.
+    We assume T is a neural network operator (from PyTorch).
 
     Example Code Snippet:
         KM  = KM_alg(S, T, alpha, device)
@@ -23,8 +24,8 @@ class KM_alg():
         self._alpha = alpha
         self._S = S
         self._T = T
-        self._max_depth = max_depth
-        self._eps = eps
+        self.max_depth = max_depth
+        self.eps_tol = eps
         self._device = device
 
     def __repr__(self):
@@ -34,12 +35,22 @@ class KM_alg():
         output += '      default eps = %r\n'
         output += '      device      = %r\n'
         output += ')'
-        return output % (self._alpha, self._max_depth, self._eps, self._device)
+        return output % (self._alpha, self.max_depth,
+                         self.eps_tol, self._device)
 
     def __call__(self, u: torch.tensor,
                  d: torch.tensor, eps=-1) -> torch.tensor:
+        """ Apply the KM algorithm.
 
-        eps = eps if eps > 0 else self._eps
+            Training: Use this for forward prop, but do NOT use
+                      is for back prop.
+
+            Note: Because u contains batches of samples, we loop
+                  until every sample converges, unless we hit the
+                  max depth/number of iterations.
+        """
+
+        eps = eps if eps > 0 else self.eps_tol
         depth = 0.0
         u_prev = u.clone()
         indices = np.array(range(len(u[:, 0])))
@@ -47,26 +58,24 @@ class KM_alg():
         # Mask identifies not converged 'nc' samples (False = converged)
         nc = np.ones((1, u[:, 0].size()[0]), dtype=bool)
         nc = nc.reshape((nc.shape[1]))
-        with torch.no_grad():  # avoid storing grad in memory
-            # loop until all samples converge or max out iterations
-            while nc.any() > 0 and depth < self._max_depth:
+        with torch.no_grad():
+            while nc.any() > 0 and depth < self.max_depth:
                 Tu = self._T(u, d)
                 u_prev = u.clone()
                 u = self._alpha * self._S(u) + (1-self._alpha) * Tu
                 nc[nc > 0] = [torch.norm(u[i, :] - u_prev[i, :]) > eps
                               for i in indices[nc > 0]]
                 depth += 1.0
-        if depth >= self._max_depth:
+        if depth >= self.max_depth:
             print("KM: Max Depth Reached - Break Forward Loop")
         return u.to(self._device), depth
 
     def apply_T(self, u: torch.tensor, d: torch.tensor) -> torch.tensor:
         """ Detach any gradients and then create gradient graph for
-            a singular application of T. Use this in lieu of calling KM
-            for network forward prop.
+            a single application of T. This is used for backprop rather
+            than calling the KM algorithm.
         """
-        y = u.detach()
-        return self._T(y, d)
+        return self._T(u.detach(), d)
 
     def assign_ops(self, S, T):
         """ Use this to update T after every step of optimizer during training
