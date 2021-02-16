7aff955f7c73a52bd498e36bf011d245574aa839
Howard <54549555+howardheaton@users.noreply.github.com>
2021-02-15 19:57:03 -0800

    Adding CIFAR10-Unaugmented to Networks.py

--- Networks.py
+++ Networks.py
@@ -153,31 +153,35 @@ class LFPN(ABC, nn.Module):
         return self.map_latent_to_inference(u)
 
 
-class MNIST_CNN(LFPN):
-    def __init__(self, lat_dim, device, s_hi=1.0):
+class CIFAR10_CNN(LFPN):
+    def __init__(self, lat_dim, device, s_hi=1.0, inf_dim=10):
         super().__init__()
         self.maxpool = nn.MaxPool2d(kernel_size=2)
         self.relu = nn.ReLU()
-        self.fc_y = nn.Linear(1250,    lat_dim, bias=False)
-        self.fc_u = nn.Linear(lat_dim, lat_dim, bias=False)
         self._lat_dim = lat_dim
-        self._device = device
+        self._inf_dim = inf_dim
         self._s_hi = s_hi
-        self.drop_out = nn.Dropout(p=0.2)
-        self.soft_max = nn.Softmax(dim=1)
-        self.conv1 = torch.nn.utils.spectral_norm(nn.Conv2d(in_channels=1,
-                                                            out_channels=85,
-                                                            kernel_size=3,
-                                                            stride=1),
-                                                  n_power_iterations=20)
-        self.conv2 = torch.nn.utils.spectral_norm(nn.Conv2d(in_channels=85,
-                                                            out_channels=50,
-                                                            kernel_size=3,
-                                                            stride=1),
-                                                  n_power_iterations=20)
+        self._device = device
+        self.dropout = nn.Dropout(p=0.5)
+
+        self._dim_out1 = 80
+        self._dim_out2 = 90
+        self._dim_out3 = 100
+
+        self.fc_u = nn.Linear(lat_dim, lat_dim, bias=False)
+        self.conv1 = nn.Conv2d(in_channels=3, out_channels=self._dim_out1,
+                               kernel_size=3, stride=1)
+        self.conv2 = nn.Conv2d(in_channels=self._dim_out1,
+                               out_channels=self._dim_out2, kernel_size=3,
+                               stride=1)
+        self.conv3 = nn.Conv2d(in_channels=self._dim_out2,
+                               out_channels=self._dim_out3, kernel_size=3,
+                               stride=1)
+        self.fc_v = nn.Linear(in_features=self._dim_out3 * 4,
+                              out_features=lat_dim, bias=True)
 
     def name(self):
-        return 'MNIST_CNN'
+        return 'CIFAR10_CNN'
 
     def device(self):
         return self._device
@@ -189,17 +193,15 @@ class MNIST_CNN(LFPN):
         return self._s_hi
 
     def latent_space_forward(self, u, v):
-        u = self.fc_u(0.5 * self.relu(u) + 0.5 * v)
-        y = self.soft_max(u[:, 0:10].clone())
-        u[:, 0:10] = y
-        return u
+        return self.fc_u(0.99 * self.relu(u) + v)
 
     def data_space_forward(self, d):
+        current_batch_size = d.shape[0]
         v = self.maxpool(self.relu(self.conv1(d)))
-        v = self.maxpool(self.relu(self.drop_out(self.conv2(v))))
-        v = v.view(d.shape[0], -1)
-        v = self.relu(self.fc_y(v))
-        return v
+        v = self.maxpool(self.relu(self.dropout(self.conv2(v))))
+        v = self.maxpool(self.relu(self.conv3(v)))
+        v = v.view(current_batch_size, -1)
+        return self.relu(self.fc_v(v))
 
     def map_latent_to_inference(self, u):
         return u[:, 0:10]
